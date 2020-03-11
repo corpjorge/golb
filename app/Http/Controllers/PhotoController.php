dd7c74cba9689a39e1345f46385425a73825d818
Jorge Eduardo <corpjorge@hotmail.com>
2020-03-11 16:30:47 -0500

    Controller

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Photo;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Psr\Http\Message\ServerRequestInterface;
+
+class PhotoController extends Controller
+{
+    /**
+     * The user repository instance.
+     */
+    protected $users;
+
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @param $users
+     */
+    public function __construct($users = NULL)
+    {
+        $this->middleware('auth');
+
+        //$this->middleware('log')->only('index');
+
+        //$this->middleware('subscribed')->except('store');
+
+        $this->users = $users;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function index(Request $request)
+    {
+        dd($request->flash());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Photo $photo
+     * @return void
+     */
+    public function show(Photo $photo)
+    {
+        echo 'dd';
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Photo $photo
+     * @return Response
+     */
+    public function edit(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param Photo $photo
+     * @return Response
+     */
+    public function update(Request $request, Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Photo $photo
+     * @return Response
+     */
+    public function destroy(Photo $photo)
+    {
+        //
+    }
+}
